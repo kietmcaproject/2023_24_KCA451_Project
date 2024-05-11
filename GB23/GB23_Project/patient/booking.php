b3bed841cabe3c6245f744fd42e71bfd01231acb
RhythmDhingra <145742610+RhythmDhingra@users.noreply.github.com>
2024-05-11 11:08:06 +0530

    Update booking.php

--- GB23/GB23_Project/patient/booking.php
+++ GB23/GB23_Project/patient/booking.php
@@ -257,7 +257,7 @@
                                                             Session Title: '.$title.'<br>
                                                             Session Scheduled Date: '.$scheduledate.'<br>
                                                             Session Starts : '.$scheduletime.'<br>
-                                                            Channeling fee : <b>LKR.2 000.00</b>
+                                                            Channeling fee : <b>Rs.200</b>
 
                                                         </div>
                                                         <br>
