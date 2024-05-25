75eb84c9c82324ce1d86bf3d7cb229b193c2ac63
mansipanwar1027 <152358782+mansipanwar1027@users.noreply.github.com>
2024-05-25 10:36:08 +0530

    Add files via upload

---
+++ GB17/bank management system/src/bank/management/system/Conn.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bank.management.system;
+
+/**
+ *
+ * @author HP
+ */
+import java.sql.*;  
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","PHW#84#jeor");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
